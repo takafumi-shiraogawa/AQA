ba3c9a1d3024218b5996f5ddb840ce47d6e33be9
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2023-04-11 20:09:23 +0900

    fix the geometrical response matrix in KSDFT

--- Alchemical_CP_perturbator_pyscf22/alch_deriv_ksdft.py
+++ Alchemical_CP_perturbator_pyscf22/alch_deriv_ksdft.py
@@ -1,13 +1,11 @@
-from pyscf import lib
 import mod_hessian_rhf
-import numpy
 
-def proc_hessian_(mf, mf_hess, mo_energy=None, mo_coeff=None, mo_occ=None, h1ao_grad=None):
+def proc_hessian_(mf_hess, mo_energy=None, mo_coeff=None, mo_occ=None, h1ao_grad=None):
     """
     Process hessian evaluation for alchemical force
     Modified proc_hessian_ in pyscf/prop/infrared/rhf
     """
-    # mf = mf_hess.base
+    mf = mf_hess.base
 
     mo_energy = mo_energy if mo_energy else mf.mo_energy
     mo_coeff = mo_coeff if mo_coeff else mf.mo_coeff
@@ -15,17 +13,19 @@ def proc_hessian_(mf, mf_hess, mo_energy=None, mo_coeff=None, mo_occ=None, h1ao_
     h1ao_grad = h1ao_grad if h1ao_grad else mf_hess.make_h1(mo_coeff, mo_occ)
 
     # moao1_grad, mo_e1_grad = mf_hess.solve_mo1(mo_energy, mo_coeff, mo_occ, h1ao_grad)
-    moao1_grad, mo_e1_grad = mf_hess.get_response_matrix(mo_energy, mo_coeff, mo_occ, h1ao_grad)
+    mo1, mo_e1_grad = mf_hess.get_response_matrix(mo_energy, mo_coeff, mo_occ, h1ao_grad)
 
-    hess_elec = mf_hess.hess_elec(mo1=moao1_grad, mo_e1=mo_e1_grad, h1ao=h1ao_grad)
-    hess_nuc = mf_hess.hess_nuc()
-    mf_hess.de = hess_elec + hess_nuc
-    mo1_grad = lib.einsum("up, uv, Axvi -> Axpi", mo_coeff, mf.get_ovlp(), moao1_grad)
-    return mf_hess, h1ao_grad, mo1_grad, mo_e1_grad
+    # hess_elec = mf_hess.hess_elec(mo1=moao1_grad, mo_e1=mo_e1_grad, h1ao=h1ao_grad)
+    # hess_nuc = mf_hess.hess_nuc()
+    # mf_hess.de = hess_elec + hess_nuc
+    # mo1_grad = lib.einsum("up, uv, Axvi -> Axpi", mo_coeff, mf.get_ovlp(), moao1_grad)
+    # return mf_hess, h1ao_grad, mo1_grad, mo_e1_grad
+
+    return mo1
 
 def make_U_R(mf):
-    # TODO: More elegant coding is possible by using base with PySCF 2.
-    # mf.mf_hess = mod_hessian_rhf.Hessian(mf.base)
-    mf_hess = mod_hessian_rhf.Hessian(mf)
-    # mf_hess, h1ao_grad, mo1_grad, mo_e1_grad = proc_hessian_(mf.mf_hess)
-    mf_hess, h1ao_grad, mo1_grad, mo_e1_grad = proc_hessian_(mf, mf_hess)
+    """ Calculate the first-order response matrix with respect to nuclear coordinates """
+
+    mf.mf_hess = mod_hessian_rhf.Hessian(mf)
+    mo1 = proc_hessian_(mf.mf_hess)
+    return mo1
